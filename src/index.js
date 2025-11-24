298685ef5a70f1a6091650d255c9d33d53609973
ekene <nwobodoe71@gmail.com>
2025-11-24 14:09:21 +0100

    fix: module not found

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const transactionRoutes = require("./routes/transactions");
+const transactionRoutes = require("./routes/transaction");
 
 dotenv.config();
 
